b11a9e6615a0dd8d86ca83d00e9f102bd8654cbb
Andrew-webdev <andreyrow96@gmail.com>
2020-03-15 12:40:51 +0300

    feat: add slider

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//  Active-menu 
+
 const MENU = document.getElementById('menu');
 
 
@@ -8,11 +10,11 @@ MENU.addEventListener('click', (event) => {
 
 //  Phone Click
 
-let verticalBtn = document.getElementById('vertical-phone-btn');
-let verticalDisplay = document.getElementById('vertical-phone-display');
+const verticalBtn = document.getElementById('vertical-phone-btn');
+const verticalDisplay = document.getElementById('vertical-phone-display');
 
-let horizontalBtn = document.getElementById('horizontal-phone-btn');
-let horizontalDisplay = document.getElementById('horizontal-phone-display')
+const horizontalBtn = document.getElementById('horizontal-phone-btn');
+const horizontalDisplay = document.getElementById('horizontal-phone-display');
 
 let countClickVertical = 0;
 let countClickHorizontal = 0;
@@ -21,21 +23,57 @@ let countClickHorizontal = 0;
 verticalBtn.onclick = function () {
     countClickVertical++;
     if (countClickVertical % 2 == 0) {
-        verticalDisplay.classList.remove('slider__item-horizontal-phone-display-hidden')
-        console.log(countClickVertical)
+        verticalDisplay.classList.remove('slider__item-phone-display-hidden');
     } else {
-        verticalDisplay.classList.add('slider__item-horizontal-phone-display-hidden')
-        console.log(countClickVertical)
+        verticalDisplay.classList.add('slider__item-phone-display-hidden');
     }
 }
 
 horizontalBtn.onclick = function () {
     countClickHorizontal++;
     if (countClickHorizontal % 2 == 0) {
-        horizontalDisplay.classList.remove('slider__item-horizontal-phone-display-hidden')
-        console.log(countClickHorizontal)
+        horizontalDisplay.classList.remove('slider__item-phone-display-hidden');
     } else {
-        horizontalDisplay.classList.add('slider__item-horizontal-phone-display-hidden')
-        console.log(countClickHorizontal)
+        horizontalDisplay.classList.add('slider__item-phone-display-hidden');
+    }
+}
+
+//  SLIDER 
+const slider = document.getElementById('slider')
+const slide_1 = document.getElementById('slide-1');
+const slide_2 = document.getElementById('slide-2')
+
+const leftArrow = document.getElementById('slider-left-arrow');
+const rightArrow = document.getElementById('slider-right-arrow');
+
+let countClickArrow = 0;
+
+leftArrow.onclick = function () {
+    countClickArrow++;
+    if (countClickArrow % 2 == 0) {
+        slide_1.querySelector('.slider__item-vertical-phone').classList.remove('slider-none');
+        slide_1.querySelector('.slider__item-horizontal-phone').classList.remove('slider-none');
+        slide_2.classList.add('slider-none');
+        slider.classList.remove('slide-2__bg');
+    } else {
+        slide_1.querySelector('.slider__item-vertical-phone').classList.add('slider-none');
+        slide_1.querySelector('.slider__item-horizontal-phone').classList.add('slider-none');
+        slide_2.classList.remove('slider-none');
+        slider.classList.add('slide-2__bg');
+    }
+}
+
+rightArrow.onclick = function () {
+    countClickArrow++;
+    if (countClickArrow % 2 == 0) {
+        slide_1.querySelector('.slider__item-vertical-phone').classList.remove('slider-none');
+        slide_1.querySelector('.slider__item-horizontal-phone').classList.remove('slider-none');
+        slide_2.classList.add('slider-none');
+        slider.classList.remove('slide-2__bg');
+    } else {
+        slide_1.querySelector('.slider__item-vertical-phone').classList.add('slider-none');
+        slide_1.querySelector('.slider__item-horizontal-phone').classList.add('slider-none');
+        slide_2.classList.remove('slider-none');
+        slider.classList.add('slide-2__bg');
     }
 }
